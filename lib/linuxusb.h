dc109e38a3b70149955969f45476fdd8768f8a5b
robincornelius <robincornelius>
2007-02-28 11:55:23 +0000

    fix linux build

--- lib/linuxusb.h
+++ lib/linuxusb.h
@@ -1,8 +1,11 @@
 
-#define HANDLEGM int
+#include "gm0.h"
 
 void polldata (HANDLEGM hand);
+
 unsigned char WritepacketToDevice(HANDLEGM hand, unsigned char cmd, unsigned char data);
+
 BOOL FindTheHID(HANDLEGM hand);
+
 
 
