49776c0f36e2cd0b4aa5f324dc367c1a8f1e8a69
robincornelius <robincornelius>
2007-02-28 11:23:36 +0000

    Update makefile and USB linux stubs

---
+++ lib/linuxusb.c
@@ -0,0 +1,26 @@
+
+
+#include "gm0.h"
+
+void polldata(HANDLEGM hand)
+{
+
+
+}
+
+unsigned char WritepacketToDevice(HANDLEGM hand, unsigned char cmd, unsigned char data)
+{
+
+
+
+    return 0;
+}
+
+BOOL FindTheHID(HANDLEGM hand)
+{
+
+
+    return FALSE;
+}
+
+
